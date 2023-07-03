298cbb5f96c176f3a292e2be166bff158341f43d
azumi-nozaki <azumi-nozaki@kccs.co.jp>
2023-07-03 13:36:00 +0900

    [mod]Hello.java
    引数ありコンストラクタの生成

--- GitTutorial/src/model/Hello.java
+++ GitTutorial/src/model/Hello.java
@@ -1,9 +1,13 @@
 package model;
 
-public class Hello implements  {
+import java.io.Serializable;
+
+public class Hello implements Serializable {
 private String name;
 
-public Hello() {
-
+public Hello() {}
+public Hello(String name) {
+this.name=name;
 }
+
 }
